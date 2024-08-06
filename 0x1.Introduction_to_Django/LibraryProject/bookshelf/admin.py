abf439ccc9b509768e354dd49cf127b1aa4924c0
Janassalem <janamohamed248@gmail.com>
2024-08-06 13:35:48 +0300

    t

--- 0x1.Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin # type: ignore
 from .models import Book
 
-admin.site.register(Book)
-from django.contrib import admin # type: ignore
-from .models import Book
-
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
