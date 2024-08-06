0b742ada804e7b117da7609b066d0f624f09a7f6
Janassalem <janamohamed248@gmail.com>
2024-08-06 13:31:22 +0300

    LAST COMMIT

--- 0x1.Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,3 +2,12 @@ from django.contrib import admin # type: ignore
 from .models import Book
 
 admin.site.register(Book)
+from django.contrib import admin # type: ignore
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
