f5c40e3ba3475a071ef4ed65b0f89f3782a62714
Janassalem <janamohamed248@gmail.com>
2024-08-12 23:09:13 +0300

    fucking

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,7 +5,7 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE , related_name='Author')
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
