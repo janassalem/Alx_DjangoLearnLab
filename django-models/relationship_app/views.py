f6835357b5dfb177e8df545ef3a592bf5b268087
Janassalem <janamohamed248@gmail.com>
2024-08-13 00:39:52 +0300

    dmksdkd

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import register
 from django.views.generic.detail import DetailView
 from django.shortcuts import render, get_object_or_404
 from .models import Library
