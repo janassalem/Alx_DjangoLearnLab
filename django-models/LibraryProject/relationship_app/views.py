dc33acb2db5daeb40c5b00ca5a82eff4c012f8e8
Janassalem <janamohamed248@gmail.com>
2024-08-12 14:08:30 +0300

    hi

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,3 +11,9 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
+from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.select_related('author').all()  # Optimized query to fetch related authors
+    return render(request, 'list_books.html', {'books': books})
