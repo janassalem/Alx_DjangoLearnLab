27b41cf501fc16f0f3e0d1c038450276b7dacccf
Janassalem <janamohamed248@gmail.com>
2024-08-12 22:53:41 +0300

    nh

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,4 @@
-from django.db import models # type: ignore
+from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -8,10 +8,17 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    Author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
