1baad6825bb1f085ea2b4e02c8a9ec70c1a2de0b
Janassalem <janamohamed248@gmail.com>
2024-08-13 00:23:47 +0300

    msxsmx

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Library
+
 from django.shortcuts import render
 from relationship_app.models import Book
 
@@ -9,9 +12,13 @@ from django.views.generic import DetailView
 from relationship_app.models import Library
 
 class LibraryDetailView(DetailView):
+   class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+   
 
 from django.shortcuts import render
 from .models import Book
