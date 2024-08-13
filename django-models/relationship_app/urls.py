4474dd8096e9a161759b700aca9d3139858e21e0
Janassalem <janamohamed248@gmail.com>
2024-08-13 11:32:02 +0300

    lovedive

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -21,3 +21,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
+urlpatterns = [
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('admin/', views.admin_view, name='admin_view'),
+]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -72,3 +72,18 @@ from django.shortcuts import render
 
 def register(request):
     return render(request, 'relationship_app/register.html')
+from django.contrib.auth.decorators import user_passes_test
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
