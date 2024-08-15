9a7693ad89067005d68c76255ca945321ae8dbbd
Janassalem <janamohamed248@gmail.com>
2024-08-15 10:32:44 +0300

    nnn

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -62,3 +62,9 @@ urlpatterns = [
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
 ]
+from django.urls import path
+from .views import admin_view
+
+urlpatterns = [
+    path('admin/', admin_view, name='admin_view'),
+]
