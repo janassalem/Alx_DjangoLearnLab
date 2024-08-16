7eaf1b2db7c013617f1d791134bfbc04cbd5bf29
Janassalem <janamohamed248@gmail.com>
2024-08-16 23:42:07 +0300

    ms,sdm

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -11,3 +11,17 @@ urlpatterns = [
     path("edit_book/<int:book_id>/", edit_book, name="edit-book"),
     path("delete_book/<int:book_id>/", delete_book, name="delete-book"),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("list_books/", views.list_books, name="list-books"),
+    path("library_detail/", views.LibraryDetailView.as_view(), name="library-detail"),
+    path("register/", views.register, name="register"),
+    path("login/", views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+    path("add_book/", views.add_book, name="add-book"),
+    path("edit_book/<int:book_id>/", views.edit_book, name="edit-book"),
+    path("delete_book/<int:book_id>/", views.delete_book, name="delete-book"),
+]
