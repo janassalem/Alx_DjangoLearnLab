696e033524dd312f6a07436ceefb874edefc580f
Janassalem <janamohamed248@gmail.com>
2024-08-16 23:37:22 +0300

    dmdms

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,20 +1,15 @@
-from relationship_app.models import Author, Book
+from .models import Book, Author, Library, Librarian
 
-def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author=author)
-    return books
+# Query all books by a specific author.
+author_name = "John Doe"
+author = Author.objects.get(name=author_name)
+books = Book.objects.filter(author=author)
 
-from relationship_app.models import Library
+# List all books in a library.
+books = Library.objects.get(name=library_name)
+books.all()
 
-def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    books = library.books.all()
-    return books
-
-from relationship_app.models import Library, Librarian
-
-def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    librarian = Librarian.objects.get(library=library)
-    return librarian
+# Retrieve the librarian for a library
+library_name = "Prempeh library"
+library = Library.objects.get(name=library_name)
+librarian = Librarian.objects.get(library=library)
--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,36 +1,13 @@
 from django.urls import path
-from .views import list_books
-from .views import LibraryDetailView
-from .views import register
-from .views import LoginView
-from .views import LogoutView
-from . import views
+from .views import list_books, LibraryDetailView, register, LoginView, LogoutView, add_book, edit_book, delete_book
 
 urlpatterns = [
     path("list_books/", list_books, name="list-books"),
-    path("library_detail/", views.LibraryDetailView.as_view(), name="library-detail"),
-    path("register/", views.register, name="register"),
-    path("login/", views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
-    path("logout/", views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("add_book/", views.add_book, name="add-book"),
-    path("edit_book/<int:book_id>/", views.edit_book, name="edit-book"),
-    path("delete_book/<int:book_id>/", views.delete_book, name="delete-book"),
-]
-from django.urls import path
-from .views import list_books
-from .views import LibraryDetailView
-from .views import register
-from .views import LoginView
-from .views import LogoutView
-from . import views
-
-urlpatterns = [
-    path("list_books/", list_books, name="list-books"),
-    path("library_detail/", views.LibraryDetailView.as_view(), name="library-detail"),
-    path("register/", views.register, name="register"),
-    path("login/", views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
-    path("logout/", views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("add_book/", views.add_book, name="add-book"),
-    path("edit_book/<int:book_id>/", views.edit_book, name="edit-book"),
-    path("delete_book/<int:book_id>/", views.delete_book, name="delete-book"),
+    path("library_detail/", LibraryDetailView.as_view(), name="library-detail"),
+    path("register/", register, name="register"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+    path("add_book/", add_book, name="add-book"),
+    path("edit_book/<int:book_id>/", edit_book, name="edit-book"),
+    path("delete_book/<int:book_id>/", delete_book, name="delete-book"),
 ]
