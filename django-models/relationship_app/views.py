21462c10ee83673e5752f4ef4a9d121d8972c60a
Janassalem <janamohamed248@gmail.com>
2024-08-17 02:19:47 +0300

    saldas,d

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -134,3 +134,19 @@ def librarian_view(request):
 def book_list(request):
     form = BookForm()
     return render(request, 'relationship_app/book_list.html', {'form': form})
+
+
+def check_role(user, role):
+  return user.is_authenticated and user.userprofile.role == role
+
+@user_passes_test(lambda user: check_role(user, "Admin"))
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+@user_passes_test(lambda user: check_role(user, "Librarian"))
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+@user_passes_test(lambda user: check_role(user, "Member"))
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
