966105fc3a87ce903f81b954debf5e5607418034
Janassalem <janamohamed248@gmail.com>
2024-08-19 17:01:43 +0300

    wjwns

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm  # Ensure this form is defined in forms.py
+from .forms import BookForm
+from .forms import ExampleForm  # Ensure this form is defined in forms.py
 # Create your views here.
 @permission_required('bookshelf.can_view', raise_exception=True)
 def view_content(request):
@@ -41,4 +42,19 @@ def add_book(request):
             return redirect('book_list')
     else:
         form = BookForm()
+    return render(request, 'bookshelf/add_book.html', {'form': form})
+
+def book_list(request):
+    # Securely querying all books from the database
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+def add_book(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')  # Redirect to the book list after successful submission
+    else:
+        form = ExampleForm()
     return render(request, 'bookshelf/add_book.html', {'form': form})
