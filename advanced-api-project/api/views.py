b9d1c83432ece8851bdbeea815a73e9dfa58c12f
Janassalem <janamohamed248@gmail.com>
2024-09-04 17:13:07 +0300

    b

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .seriealizers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
@@ -10,3 +11,11 @@ class BookListView(generics.ListCreateAPIView):
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    # Custom methods or overrides if needed
+
