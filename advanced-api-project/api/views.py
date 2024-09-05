1d7b87047fee67fbfc4b0f194fea8806894c92ea
Janassalem <janamohamed248@gmail.com>
2024-09-05 17:29:56 +0300

    s

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.filters import OrderingFilter
+from django_filters import rest_framework
 
 
  
