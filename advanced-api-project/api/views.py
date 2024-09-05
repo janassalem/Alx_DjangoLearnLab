d31f7ef09b027cca76d374063e636e392e5cbb8d
Janassalem <janamohamed248@gmail.com>
2024-09-05 18:19:31 +0300

    a

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Book
-from .seriealizers import BookSerializer
+from .serializers import BookSerializer
 from django_filters import rest_framework
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
