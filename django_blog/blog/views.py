0e417f1767af303ccd79f3531c216259d4918de8
Janassalem <janamohamed248@gmail.com>
2024-09-12 13:56:38 +0300

    n

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -31,3 +31,56 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def profile(request):
     return render(request, 'blog/profile.html')
+
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+
+# List View for all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # Template for listing posts
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # Order posts by newest first
+
+# Detail View for a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+# Create View for authenticated users
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']  # Fields to display in the form
+    template_name = 'blog/post_form.html'
+
+    # Override form_valid to set the author to the logged-in user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# Update View for post authors only
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # Ensure only the post author can edit
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# Delete View for post authors only
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/'  # Redirect to the home page after deletion
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
