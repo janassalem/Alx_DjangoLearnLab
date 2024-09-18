294a333d2a70629895ffd23208a75fc4c6fd5be7
Janassalem <janamohamed248@gmail.com>
2024-09-18 16:59:31 +0300

    u

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,4 +1,33 @@
 from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid login credentials")
+
+from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
@@ -16,15 +45,16 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password']
 
+    # The `create` method is overridden to use get_user_model().objects.create_user()
     def create(self, validated_data):
-        # Create a new user instance using the get_user_model method
+        # Create a new user instance using get_user_model().objects.create_user()
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
         
-        # Create a token for the user
+        # Create a token for the user upon successful registration
         Token.objects.create(user=user)
         
         return user
