0e1f38386dd0f62f08a38e99c96c427d3f351ca2
Janassalem <janamohamed248@gmail.com>
2024-09-18 16:58:01 +0300

    f

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,27 +1,41 @@
 from rest_framework import serializers
-from .models import CustomUser
-from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+
+# Get the custom user model
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        # Create a new user instance using the get_user_model method
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        
+        # Create a token for the user
+        Token.objects.create(user=user)
+        
         return user
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
 
     def validate(self, data):
+        # Authenticate the user
         user = authenticate(**data)
         if user and user.is_active:
             return user
